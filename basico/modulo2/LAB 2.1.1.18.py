435970dc80d195591e910cf4b1f67e50024004bd
DanielFHM <dhernandezmanzano97@gmail.com>
2022-06-14 17:48:11 -0500

    curso

---
+++ basico/modulo2/LAB 2.1.1.18.py
@@ -0,0 +1,4 @@
+# 2.1.1.18 LABORATORIO: La función print()
+
+print("Fundamentos","Programación","en", sep="***", end="...")
+print("Python")
---
+++ basico/modulo2/LAB 2.1.1.19.py
@@ -0,0 +1,58 @@
+# 2.1.1.19 LABORATORIO: Dando formato a la salida
+
+# Solución de Muestra
+
+###################
+print("Versión Original:")
+###################
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+###################
+print("Con menos invocaciones de 'print()': ")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("Más alto:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("Doble:")
+###################
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
