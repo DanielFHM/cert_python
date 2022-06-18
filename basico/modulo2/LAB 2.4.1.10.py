e2cf4a711041e6e787f793ac14acf0a744b90b79
DanielFHM <dhernandezmanzano97@gmail.com>
2022-06-17 20:28:32 -0500

    curso

---
+++ basico/modulo2/LAB 2.4.1.10.py
@@ -0,0 +1,16 @@
+# 2.4.1.10 LABORATORIO: Operadores y expresiones
+
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
---
+++ basico/modulo2/LAB 2.6.1.10.py
@@ -0,0 +1,7 @@
+# 2.6.1.10 LABORATORIO: Operadores y expresiones
+
+x = float(input("Ingresa un valor para x: "))
+
+y = 1./(x + 1./(x + 1./(x + 1./x)))
+
+print("y =", y)
