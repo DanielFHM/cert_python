4e4a551dc1522857d9b1bbf5696aec112075ce2b
DanielFHM <dhernandezmanzano97@gmail.com>
2022-06-21 16:39:09 -0500

    curso

---
+++ basico/modulo3/LAB 3.4.1.6.py
@@ -0,0 +1,12 @@
+#  3.4.1.6 LABORATORIO: Lo básico de las listas 
+
+hat_list = [1, 2, 3, 4, 5]
+
+# Paso 1
+hat_list[2] = int(input("Ingresa un número entero: "))
+
+# Paso 2
+del hat_list[-1]
+
+# Paso 3
+print(len(hat_list))
